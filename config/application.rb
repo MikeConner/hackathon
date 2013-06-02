8cad102e53a370bce8121b287efe4b5353ef5b7e
Bruce McElroy <bruce.mcelroy@gmail.com>
2013-06-01 22:46:46 -0400

    some udpates n @

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module Hackathon
     config.assets.logger = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.2'
   end
 end
